e0c8408d3a636a6f486c08a04826e5df992a6554
ArkcV <andrielmarques@hotmail.com>
2025-05-09 15:45:43 -0300

    feat: add Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from "react";
+import "./Navbar.css";
+import {
+  MdOutlineDeliveryDining,
+  MdSearch,
+  MdShoppingBasket,
+} from "react-icons/md";
 
 const Navbar = () => {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+  const [menu, setMenu] = useState("home");
 
-export default Navbar
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        <MdOutlineDeliveryDining className="navbar-logo-icon" />
+        <span className="navbar-logo-title">Food Delivery</span>
+      </div>
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          inicio
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact us")}
+          className={menu === "contact us" ? "active" : ""}
+        >
+          contato-nos
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <MdSearch className="navbar-search" />
+        <div className="navbar-search-icon">
+          <MdShoppingBasket className="navbar-shopbasket" />
+          <div className="dot"></div>
+        </div>
+        <button>entrar</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
