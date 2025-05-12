bfa636e6315b8ecb35abfce29e1b62143f04bd90
ArkcV <andrielmarques@hotmail.com>
2025-05-12 10:37:10 -0300

    feat: add Header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Peça aqui suas comidas favoritas</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nam
+          alias nulla molestiae commodi sit. Obcaecati impedit nisi nihil magnam
+          at a, suscipit rerum dolore assumenda nobis laudantium.
+          Exercitationem, odio?
+        </p>
+         <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
